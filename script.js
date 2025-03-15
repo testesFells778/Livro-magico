99820526bfdafe80f7e7c27ebfa43d04bfb51275
Felipe Souza <96696142+Fells778@users.noreply.github.com>
2025-03-14 22:41:28 -0300

    alteração no mediaquery

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
+    function resizeBook() {
+        let screenWidth = $(window).width();
+        let bookWidth, bookHeight;
+
+        if (screenWidth < 600) {
+            bookWidth = screenWidth * 0.9; // 90% da tela
+            bookHeight = bookWidth * 0.75; // Mantendo proporção
+        } else {
+            bookWidth = 900; // Tamanho fixo para telas grandes
+            bookHeight = 700;
+        }
+
+        $(".flipbook").turn("size", bookWidth, bookHeight);
+    }
+
     $(".flipbook").turn({
-        width: 900,
-        height: 700,
-        autoCenter: true,
-        display: "double",
-        elevation: 50,
-        gradients: true,
+        width: 600,
+        height: 840,
+        autoCenter: true
     });
+
+    $(window).on("resize", resizeBook);
+    resizeBook();
 
     $("#prev").click(function () {
         $(".flipbook").turn("previous");
